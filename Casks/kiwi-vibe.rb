18dc23e4ab1d3cae488bda31a33d6c8fef4f335d
EslavaDev <daniel.eslava@kiwicredito.com>
2026-04-01 17:38:49 -0500

    feat: update cask to v0.3.3 (Apple signed)

--- Casks/kiwi-vibe.rb
+++ Casks/kiwi-vibe.rb
@@ -1,14 +1,14 @@
 cask "kiwi-vibe" do
-  version "0.3.2"
+  version "0.3.3"
 
   on_arm do
     url "https://github.com/kiwicredito/vibe-releases/releases/download/vibe-v#{version}/KiwiVibe_#{version}_aarch64.dmg"
-    sha256 "5db6a6259af25c95d3b84d47bec740ae99ff94c1decfc339289861865507d489"
+    sha256 "8dc5fd5bf0a851baccd377d279a8830d68c9a6bcb83ef493b820aff9688bec8c"
   end
 
   on_intel do
     url "https://github.com/kiwicredito/vibe-releases/releases/download/vibe-v#{version}/KiwiVibe_#{version}_x64.dmg"
-    sha256 "a16db922f300bdae7ec6908baabfc73cd010500d19cad3b79dedfa836ab39a83"
+    sha256 "5a82a672fdaceebb6e5144ffd6408bfb6038c286d059964ae0303e393b404afa"
   end
 
   name "Kiwi Vibe"
@@ -16,12 +16,6 @@ cask "kiwi-vibe" do
   homepage "https://github.com/kiwicredito/vibe-releases"
 
   app "KiwiVibe.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/KiwiVibe.app"],
-                   sudo: true
-  end
 
   zap trash: [
     "~/Library/Application Support/com.kiwi.vibe",
