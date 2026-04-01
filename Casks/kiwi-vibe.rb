dedbed9d1f5cace6ee164399437d1228c994707a
EslavaDev <daniel.eslava@ciudadela.eu>
2026-04-01 10:33:14 -0500

    feat: add Homebrew cask for Kiwi Vibe v0.3.2

---
+++ Casks/kiwi-vibe.rb
@@ -0,0 +1,25 @@
+cask "kiwi-vibe" do
+  version "0.3.2"
+
+  on_arm do
+    url "https://github.com/kiwicredito/vibe-releases/releases/download/vibe-v#{version}/KiwiVibe_#{version}_aarch64.dmg"
+    sha256 "5db6a6259af25c95d3b84d47bec740ae99ff94c1decfc339289861865507d489"
+  end
+
+  on_intel do
+    url "https://github.com/kiwicredito/vibe-releases/releases/download/vibe-v#{version}/KiwiVibe_#{version}_x64.dmg"
+    sha256 "a16db922f300bdae7ec6908baabfc73cd010500d19cad3b79dedfa836ab39a83"
+  end
+
+  name "Kiwi Vibe"
+  desc "Vibe coding desktop app for Kiwi development"
+  homepage "https://github.com/kiwicredito/vibe-releases"
+
+  app "KiwiVibe.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.kiwi.vibe",
+    "~/Library/Caches/com.kiwi.vibe",
+    "~/Library/Preferences/com.kiwi.vibe.plist",
+  ]
+end
